a43827f7c16909f9d9837560f6d2090e8b36b86d
NaniDeagle <iipocto1deagle@gmail.com>
2026-06-14 03:36:50 +0300

    add suport for fontawesome icons to string settings widgetBuilder

--- lib/src/data/settings/all_settings.dart
+++ lib/src/data/settings/all_settings.dart
@@ -1562,7 +1562,7 @@ void registerAllSettings() {
     stringSetting(
       key: .defTags,
       getDefaultValue: () => 'rating:safe',
-      categories: [SettingCategory.tagsFilters],
+      categories: [SettingCategory.booru],
       supportsPerBooru: true,
       localization: SettingLocalization(
         title: (ctx) => ctx.loc.settings.booru.defaultTags,
@@ -1591,7 +1591,7 @@ void registerAllSettings() {
       categories: [SettingCategory.tagsFilters],
       legacyJsonKeys: const ['lovedTags'],
       navigateTo: () => const TagsFiltersPage(),
-      icon: Icons.bookmark,
+      icon: Icons.star,
       localization: SettingLocalization(
         title: (ctx) => 'Marked Tags',
       ),
